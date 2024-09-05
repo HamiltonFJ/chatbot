5fae0614908c726e413da909384cef899194203d
HamiltonFJ <hamiltonfreire@gmail.com>
2024-09-05 15:43:19 -0300

    Atualização

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-//console.log(dados);
-// loop para cada dado dentro da lista de dados
-let section=document.getElementById("resultados-pesquisa")
-console.log(section);
-let resultados =""
-for (let dado of dados)
-{    resultados+=
-    `
-    <div class="item-resultado">
-                <h2> 
-                    <a href="#" target="_blank">${dado.nome}</a> 
-                </h2>
-                   <p class="descricao-meta">${dado.descricao}</p> 
-                   <p><h4>Período de Fabricação</h4></p>
-                   <p class="periodoFabricado-meta">${dado.periodoFabricado}</p> 
-                   <p><h4> Motorização</h4></p>
-                   <p class="motor-meta">${dado.motor}</p> 
-                   <p><a href=${dado.imagem} target="_blank">Imagem</a></p>
-                   <a href=${dado.linkInformacoesCompletas} target="_blank">Mais Informações </a>
-                
-    </div>
-   `
-}
-section.innerHTML = resultados
+function pesquisar() {
+    // Obtém a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+  
+    // Inicializa uma string para armazenar os resultados
+    let resultados = "";
+  
+    // Itera sobre cada dado na lista de dados
+    for (let dado of dados) {
+      // Concatena o HTML de cada resultado à string 'resultados'
+      resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="#" target="_blank">${dado.nome}</a>
+          </h2>
+          <p class="descricao-meta">${dado.descricao}</p>
+          </div>
+      `;
+    }
+  
+    // Atribui a string 'resultados' ao conteúdo HTML da seção
+    section.innerHTML = resultados;
+  }

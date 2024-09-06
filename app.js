a9f2bffa37e04dc2e3e8f0ca7b0cf1ad2cb7d636
HamiltonFJ <hamiltonfreire@gmail.com>
2024-09-06 15:20:59 -0300

    Atualização

--- app.js
+++ app.js
@@ -4,16 +4,40 @@ function pesquisar() {
 
     // Recebe a variável do botão pesquisar
     let campoPesquisa = document.getElementById("campo-pesquisa").value;
-    console.log(campoPesquisa); 
+    
+    // seo campo pesquisa for uma string vazia nao trará nada
+    if (campoPesquisa == "") {
+       section.innerHTML="Não existem informações a pesquisar."
+       return
+    }
+    //atribui minusculas ao conteudo do campo pesquisa
+    campoPesquisa=campoPesquisa.toLowerCase()
+    //atribui minusculas ao conteudo do arquivo dados.js
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados="";
 
-    // Inicializa uma string para armazenar os resultados
-    let resultados = "";
+    let nome="";
+    let descricao="";
+    let periodoFabricado="";
+    let motor="";
+
+    
+    //let resultados = "";
 
     // Itera sobre cada dado na lista de dados
     for (let dado of dados) {
+        nome=dado.nome.toLowerCase()
+        descricao=dado.descricao.toLowerCase()
+        periodoFabricado=dado.periodoFabricado.toLowerCase()
+        motor=dado.motor.toLowerCase()
+
       //console.log(dado.nome.includes(campoPesquisa)) <= Teste de console
       //se no dado.nome incluir o campo de pesquisa, execute:
-      if (dado.nome.includes(campoPesquisa)){
+      if (nome.includes(campoPesquisa) || 
+         descricao.includes(campoPesquisa) || 
+         periodoFabricado.includes(campoPesquisa) || 
+         motor.includes(campoPesquisa))
+         {
           // cria um novo elemento 
           // Concatena o HTML de cada resultado à string 'resultados'
           resultados+=
@@ -31,7 +55,7 @@ function pesquisar() {
                         <a href=${dado.linkInformacoesCompletas} target="_blank">Mais Informações </a>                  
           </div>
           `;
-      }
+        }
       
 
       
